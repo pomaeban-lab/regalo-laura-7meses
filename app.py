5bd27684133bd7cd3f35b7bf19db9b0ffb25b511
pomaeban-lab <pomaeban@gmail.com>
2026-05-10 14:42:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,6 +7,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # Usamos el puerto que Render nos asigne, o el 5000 por defecto
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, host='127.0.0.1', port=5002)
